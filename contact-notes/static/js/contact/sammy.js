8661a34d062d3aee91a6bcaf1a53b04482e98a30
bashofmann <bhofmann@studivz.net>
2010-11-07 15:42:08 +0100

    added sammy and initial sammy app structure

--- contact-notes/static/js/contact/sammy.js
+++ contact-notes/static/js/contact/sammy.js
@@ -3,7 +3,62 @@ require.def("contact/sammy",
   function() {
     return {
       ready: function () {
-        alert("loaded")
+		var testStorage = {
+			users : [
+				{id: 1, name: 'aaaaaa'},
+				{id: 2, name: 'bbbbbb'},
+				{id: 3, name: 'cccccc'},
+				{id: 4, name: 'dddddd'}
+			],
+			notes : {
+				1: ['aaa', 'bbb', 'ccc']
+			}
+		};
+		
+        var app = $.sammy('#main', function() {
+			this.use(Sammy.Mustache, 'ms');
+			
+			var getUsers = function(context) {
+				if (! testStorage.users) {
+					return;
+				}
+				context.users = testStorage.users;
+				context.render('templates/users.ms').appendTo('#users');
+				testStorage.users = false;
+			};
+			
+			this.around(function(callback) {
+				getUsers(this);	
+				callback();
+			});
+			
+			this.get('#/', function() {
+				this.partial('templates/main.ms');
+    		});
+
+			this.get('#/user/:id', function() {
+				this.trigger('update-notes');
+    		});
+
+			this.post('#/user/:id', function() {
+				testStorage.notes[this.params['id']].push(this.params['note']);
+				this.trigger('update-notes');
+			});
+			
+			this.bind('update-notes', function() {
+				this.userId = this.params['id'];
+				if (! testStorage.notes[this.userId]) {
+					testStorage.notes[this.userId] = [];
+				}
+				this.userNotes = testStorage.notes[this.userId];
+				this.partial('templates/notes.ms');
+			});
+			
+		});
+		
+		$(function() {
+    		app.run('#/');
+  		});
       }
     };
   })
